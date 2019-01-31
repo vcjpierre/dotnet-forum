542b5d106bb7a4acfcbf504ebd419edcdfc52be5
vcjpierre <vcjpierre@gmail.com>
2019-01-31 13:13:36 -0400

    Add service layer

---
+++ LambdaForums.Data/IForum.cs
@@ -0,0 +1,19 @@
+﻿using LambdaForums.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaForums.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateforumTitle(int forumId, string newDescription);
+    }
+}
