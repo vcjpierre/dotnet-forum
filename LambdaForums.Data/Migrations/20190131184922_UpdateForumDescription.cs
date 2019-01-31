1f75af3bd3d897a29ed9c526ad0e98b69895250f
vcjpierre <vcjpierre@gmail.com>
2019-01-31 15:23:27 -0400

    Update Migrations, create ForumController

---
+++ LambdaForums.Data/Migrations/20190131184922_UpdateForumDescription.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LambdaForums.Data.Migrations
+{
+    public partial class UpdateForumDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Forums",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Forums");
+        }
+    }
+}
--- LambdaForums.Data/Models/Forum.cs
+++ LambdaForums.Data/Models/Forum.cs
@@ -7,6 +7,7 @@ namespace LambdaForums.Data.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
         public DateTime Created { get; set; }
         public string ImageUrl { get; set; }
         public virtual IEnumerable<Post> Post { get; set; }
