026d605bc8c888cb1fac519dc066e0eb95e09a4d
vcjpierre <vcjpierre@gmail.com>
2019-02-09 21:47:10 -0400

    Implement PostService and topic view

--- LambdaForums.Service/PostService.cs
+++ LambdaForums.Service/PostService.cs
@@ -1,5 +1,6 @@
 ﻿using LambdaForums.Data;
 using LambdaForums.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -40,7 +41,11 @@ namespace LambdaForums.Service
 
         public Post GetById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Posts.Where(post => post.Id == id)
+                   .Include(post => post.User)
+                   .Include(post => post.Replies).ThenInclude(reply => reply.User)
+                   .Include(post => post.Forum)
+                   .First();
         }
 
         public IEnumerable<Post> GetFilteredPost(string searchQuery)
--- LambdaForums/Controllers/PostController.cs
+++ LambdaForums/Controllers/PostController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using LambdaForums.Data;
 using LambdaForums.Data.Models;
 using LambdaForums.Models.Post;
 using LambdaForums.Models.Reply;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LambdaForums.Controllers
@@ -12,9 +14,15 @@ namespace LambdaForums.Controllers
     public class PostController : Controller
     {
         private readonly IPost _postService;
-        public PostController(IPost postService)
+        private readonly IForum _forumService;
+
+        private static UserManager<ApplicationUser> _userManager;
+
+        public PostController(IPost postService, IForum forumService, UserManager<ApplicationUser>userManager)
         {
             _postService = postService;
+            _forumService = forumService;
+            _userManager = userManager;
         }
 
         public IActionResult Index(int id)
@@ -36,6 +44,45 @@ namespace LambdaForums.Controllers
             };
 
             return View(model);
+        }
+
+        public IActionResult Create(int id)
+        {
+            var forum = _forumService.GetById(id);
+
+            var model = new NewPostModel
+            {
+                ForumName = forum.Title,
+                ForumId = forum.Id,
+                ForumImageUrl = forum.ImageUrl,
+                AuthorName = User.Identity.Name
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPost(NewPostModel model)
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = await _userManager.FindByIdAsync(userId);
+            var post = BuildPost(model, user);
+
+            await _postService.Add(post);
+            // TODO : Implement User Rating Management
+
+            return RedirectToAction("Index", "Post", post.Id);
+        }
+
+        private Post BuildPost(NewPostModel model, ApplicationUser user)
+        {
+            return new Post
+            {
+                Title = model.Title,
+                Content = model.Content,
+                Created = DateTime.Now,
+                User = user
+            };
         }
 
         private IEnumerable<PostReplyModel> BuildPostReplies(IEnumerable<PostReply> replies)
