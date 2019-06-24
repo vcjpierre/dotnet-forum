e4e219946230bc86f075a75e3f6453586816466c
Jean Pierre Velez <vcjpierre@gmail.com>
2019-06-24 13:22:17 -0400

    Register User, Upload services

--- LambdaForums.Service/ApplicationUserService.cs
+++ LambdaForums.Service/ApplicationUserService.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace LambdaForums.Service
 {
-    class ApplicationUserService : IApplicationUser
+    public class ApplicationUserService : IApplicationUser
     {
         private readonly ApplicationDbContext _context;
 
--- LambdaForums.Service/UploadService.cs
+++ LambdaForums.Service/UploadService.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using LambdaForums.Data;
 
 namespace LambdaForums.Service
 {
-    class UploadService
+    public class UploadService : IUpload
     {
     }
 }
