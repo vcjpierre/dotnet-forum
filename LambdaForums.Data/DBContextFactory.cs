d330d91d96144bd29f8cab193267a351a6d09dfe
Jean Pierre Velez <vcjpierre@gmail.com>
2021-07-10 11:58:37 -0400

    Add DBContextFactory

---
+++ LambdaForums.Data/DBContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace LambdaForums.Data
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var returnValue = new ApplicationDbContext(builder.Options);
+
+            return returnValue;
+        }
+    }
+}
